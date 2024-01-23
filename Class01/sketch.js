68c8870242dff574bd4a1a80310860edb9879900
shelbyantill <shelbyeantill@gmail.com>
2024-01-23 14:19:35 -0600

    class 01

---
+++ Class01/sketch.js
@@ -0,0 +1,9 @@
+function setup() { //creates the canvas set up- first create page initially
+  createCanvas(400, 400);
+}
+
+function draw() { //draw things into sketch
+  background(220);
+  circle(width/2, height/2, 100);
+  circle(width/2-20, height/2-20, 20);
+}
